a025562cc22eb3b2efb9e6ddca29e9ced31d557d
Artem Kovera <artemkovera@gmail.com>
2021-10-17 08:46:44 +0300

    basic allocator

---
+++ allocators/simple-allocator.h
@@ -0,0 +1,38 @@
+#pragma once
+#include<stdlib.h>
+#include<new>
+
+namespace STLL
+{
+    template<class T>
+    class SimpleAllocator
+    {
+    public:
+
+        using value_type = T;
+
+        SimpleAllocator() = default;
+
+        template <typename U>
+        SimpleAllocator(const SimpleAllocator<U> &other) noexcept
+        { 
+            (void) other; 
+        }
+        
+        T* allocate(size_t size)
+        {
+            if (auto ptr = malloc(sizeof(T) * size))
+               return static_cast<T*>(ptr);
+
+            throw std::bad_alloc();
+        }
+
+        void deallocate(T* ptr, size_t size)
+        {   
+            (void) size;
+            free(ptr);
+            ptr = nullptr;
+        }
+
+    };
+}
